337491efc621f50a198734394b518942e1371f82
= <ganna.andrushko@gmail.com>
2023-09-17 18:27:44 +0800

    start on client

---
+++ client/src/App.js
@@ -0,0 +1,15 @@
+import Header from "./components/assets/Header";
+
+
+function App() {
+  return (
+    <> 
+      <Header />
+    <div className="container">
+     <h1>Hello App</h1>
+    </div>
+    </>
+  );
+}
+
+export default App;
---
+++ client/src/components/assets/Header.jsx
@@ -0,0 +1,17 @@
+import {GrGraphQl} from 'react-icons/gr'
+
+
+export default function Header() {
+  return (
+    <div className='navbar  mb-5 p-0 '>
+      <div className='container'>
+        <a href="/" className='navbar-brand'>
+            <div className='d-flex'>
+                <GrGraphQl className='mr-4 icon'/>
+                <h1 className='accent'>Project MNMT</h1>
+            </div>
+        </a>
+      </div>
+    </div>
+  )
+}
