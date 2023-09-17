e37b5ea21804b44150382b38e7e7a83783962af6
= <ganna.andrushko@gmail.com>
2023-09-17 18:33:20 +0800

    setting up appolo

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,19 @@
 import Header from "./components/assets/Header";
+import {ApolloProvider, ApolloClient, InMemoryCache} from '@apollo/client'
 
 
+const client = new ApolloClient({
+  uri:'http://localhost:5000/graphql',
+  cache: InMemoryCache(),
+})
 function App() {
   return (
-    <> 
+    <ApolloProvider client={client}> 
       <Header />
     <div className="container">
      <h1>Hello App</h1>
     </div>
-    </>
+    </ApolloProvider>
   );
 }
 
